2874869055ba7bee8ac3e5122a6b33992088392f
0xb39 <giiguu.giigu@gmail.com>
2024-06-19 22:38:58 +0800

    made image lazy

--- task-api/src/components/include/NotFoundPage.jsx
+++ task-api/src/components/include/NotFoundPage.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 const NotFoundPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-gray-700">
-      <img src="https://github.com/SAWARATSUKI/KawaiiLogos/blob/main/ResponseCode/404%20NotFound.png?raw=true" alt="" className='w-4' draggable={false}/>
+      <img src="https://github.com/SAWARATSUKI/KawaiiLogos/blob/main/ResponseCode/404%20NotFound.png?raw=true" alt="" className='w-4' draggable={false} loading='lazy'/>
       <p className="text-lg mb-8">Sorry, the page you are looking for does not exist.</p>
-      <Link to="/" className="text-blue-500 hover:underline">
+      <Link to="/" className="text-gray-900 hover:underline">
           Go back to Home
       </Link>
   </div>
