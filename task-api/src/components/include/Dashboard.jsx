c70e52dbf762aab440621f0686a78f4434593e5f
0xb39 <giiguu.giigu@gmail.com>
2024-06-19 22:40:45 +0800

    made homepage text appear in the middle

--- task-api/src/components/include/Dashboard.jsx
+++ task-api/src/components/include/Dashboard.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 const Dashboard = () => {
   return (
-    <div className='flex justify-center w-full'>
-      <h1 className='text-3xl text-gray-800 mt-20 '>Нүүр Хуудас</h1>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-3xl text-gray-800'>Нүүр Хуудас</h1>
     </div>
     
     
