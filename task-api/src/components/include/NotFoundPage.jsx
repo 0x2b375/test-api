841246184049813cd8f6e4303b578a072e11f5eb
0xb39 <giiguu.giigu@gmail.com>
2024-06-19 22:36:17 +0800

    added image in notfoundpage

--- task-api/src/components/include/NotFoundPage.jsx
+++ task-api/src/components/include/NotFoundPage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-white bg-gray-700">
-      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+    <div className="flex flex-col items-center justify-center h-screen text-gray-700">
+      <img src="https://github.com/SAWARATSUKI/KawaiiLogos/blob/main/ResponseCode/404%20NotFound.png?raw=true" alt="" className='w-4'/>
       <p className="text-lg mb-8">Sorry, the page you are looking for does not exist.</p>
       <Link to="/" className="text-blue-500 hover:underline">
           Go back to Home
