d5949a4d5ba6fec39bb7b77e7f996c81b4a8cdd5
0xb39 <giiguu.giigu@gmail.com>
2024-06-19 22:36:46 +0800

    made image undraggable

--- task-api/src/components/include/NotFoundPage.jsx
+++ task-api/src/components/include/NotFoundPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const NotFoundPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-gray-700">
-      <img src="https://github.com/SAWARATSUKI/KawaiiLogos/blob/main/ResponseCode/404%20NotFound.png?raw=true" alt="" className='w-4'/>
+      <img src="https://github.com/SAWARATSUKI/KawaiiLogos/blob/main/ResponseCode/404%20NotFound.png?raw=true" alt="" className='w-4' draggable={false}/>
       <p className="text-lg mb-8">Sorry, the page you are looking for does not exist.</p>
       <Link to="/" className="text-blue-500 hover:underline">
           Go back to Home
