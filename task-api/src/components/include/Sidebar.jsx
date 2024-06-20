f21919f90084578c119cf5899a6d93f7e997a94e
0xb39 <giiguu.giigu@gmail.com>
2024-06-20 19:43:39 +0800

    made sidebar kinda responsive

--- task-api/src/components/include/Sidebar.jsx
+++ task-api/src/components/include/Sidebar.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = ({ sidebarToggle }) => {
   return (
-    <div className={`fixed z-20 top-0 left-0 h-full bg-slate-800 transition-all duration-300 ${sidebarToggle ? 'w-60' : 'w-0'}`}>
-      <div className={`navbar-menu relative h-full ${sidebarToggle ? 'block' : 'hidden'}`}>
+    <div className={`fixed z-20 top-0 left-0 h-full bg-slate-800 transition-all duration-300 ${sidebarToggle ? 'w-0' : 'w-40 md:w-60'}`}>
+      <div className={`navbar-menu relative h-full ${sidebarToggle ? 'hidden' : 'block'}`}>
         <nav
           id="sidebar"
-          className="flex flex-col overflow-y-auto pt-6 sm:max-w-xs lg:w-64 w-full"
+          className="flex flex-col pt-6 sm:max-w-xs lg:w-64 w-full"
         >
           <div className="px-4">
-            <ul className="mb-8 text-sm font-medium mt-5">
+            <ul className="mb-8 text-sm font-medium mt-7 text-nowrap">
               <li>
                 <Link
                   className="flex items-center rounded py-3 pl-3 pr-4 text-gray-50 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200"
