4ec3ae27bcb04559bb2959af281a3992063f42eb
0xb39 <giiguu.giigu@gmail.com>
2024-06-20 19:53:08 +0800

    made text in the center

--- task-api/src/components/include/Dashboard.jsx
+++ task-api/src/components/include/Dashboard.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 const Dashboard = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-3xl text-gray-800'>Нүүр Хуудас</h1>
+    <div className='flex flex-col h-screen'>
+      <h1 className='text-3xl text-gray-800 m-auto'>Нүүр Хуудас</h1>
     </div>
     
     
