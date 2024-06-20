e95259b9d57046e67476509fb59b06edf7972b38
0xb39 <giiguu.giigu@gmail.com>
2024-06-20 23:59:13 +0800

    made 404 image responsive

--- task-api/src/components/include/NotFoundPage.jsx
+++ task-api/src/components/include/NotFoundPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const NotFoundPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-gray-700">
-      <img src="https://github.com/SAWARATSUKI/KawaiiLogos/blob/main/ResponseCode/404%20NotFound.png?raw=true" alt="" className='w-4' draggable={false} loading='lazy'/>
+      <img src="https://github.com/SAWARATSUKI/KawaiiLogos/blob/main/ResponseCode/404%20NotFound.png?raw=true" alt="" className='md:w-4 w-8' draggable={false} loading='lazy'/>
       <p className="text-lg mb-5">Уучлаарай, таны хайсан хуудас олдсонгүй</p>
       <Link to="/" className="text-gray-900 hover:underline">
           Нүүр хуудас-руу буцах
