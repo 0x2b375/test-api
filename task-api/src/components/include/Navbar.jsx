acb79ee2be54c7dab6b27c3e4c825213ccd03ecc
0xb39 <giiguu.giigu@gmail.com>
2024-06-20 17:38:15 +0800

    fixed navbar

--- task-api/src/components/include/Navbar.jsx
+++ task-api/src/components/include/Navbar.jsx
@@ -66,52 +66,53 @@ const Navbar = ({ sidebarToggle, setSidebarToggle }) => {
           >
             Холбогдох
           </Link>
-        </nav>
-        <div className='right-side'>
-          <div className="relative" onBlur={closeDropdown}>
-              <button
-                onClick={toggleDropdown}
-                className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200"
-              >
-                <span>Профайл</span>
-                <svg
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  className={`inline h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1 ${dropdownOpen ? 'rotate-180' : 'rotate-0'}`}
+          <div className={`right-side`}>
+            <div className="relative" onBlur={closeDropdown}>
+                <button
+                  onClick={toggleDropdown}
+                  className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-              {dropdownOpen && (
-                <div className="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-                  <div className="px-2 py-2 bg-white rounded-md shadow">
-                    <Link
-                      className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg text-black hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                      to="/link1"
-                    >
-                      Нэвтрэх
-                    </Link>
-                    <Link
-                      className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg text-black hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                      to="/link2"
-                    >
-                      Бүртгүүлэх
-                    </Link>
-                    <Link
-                      className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg text-black hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                      to="/link3"
-                    >
-                      Гарах
-                    </Link>
+                  <span>Профайл</span>
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    className={`inline h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1 ${dropdownOpen ? 'rotate-180' : 'rotate-0'}`}
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+                {dropdownOpen && (
+                  <div className="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
+                    <div className="px-2 py-2 bg-slate-800 rounded-md shadow">
+                      <Link
+                        className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg text-gray-50 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200"
+                        to="/link1"
+                      >
+                        Нэвтрэх
+                      </Link>
+                      <Link
+                        className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg text-gray-50 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200"
+                        to="/link2"
+                      >
+                        Бүртгүүлэх
+                      </Link>
+                      <Link
+                        className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg text-gray-50 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200"
+                        to="/link3"
+                      >
+                        Гарах
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              )}
+                )}
+            </div>
           </div>
-        </div>
+        </nav>
+        
       </div>
     </div>
   );
