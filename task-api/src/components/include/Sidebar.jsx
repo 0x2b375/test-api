0c0641075301c67bf3d8b59b172925c0c3efe306
0xb39 <giiguu.giigu@gmail.com>
2024-06-21 12:11:17 +0800

    added various things on dashboard page

--- task-api/src/components/include/Sidebar.jsx
+++ task-api/src/components/include/Sidebar.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons/faHome'
-import { faCircleInfo } from '@fortawesome/free-solid-svg-icons/faCircleInfo'
-import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone'
-import { faMicrochip } from '@fortawesome/free-solid-svg-icons/faMicrochip'
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome';
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons/faCircleInfo';
+import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone';
+import { faMicrochip } from '@fortawesome/free-solid-svg-icons/faMicrochip';
+
 const Sidebar = ({ sidebarToggle }) => {
   return (
-    <div className={`fixed z-20 top-0 left-0 h-full bg-slate-800 transition-all duration-300 ${sidebarToggle ? 'w-40 md:w-80' : 'w-0'}`}>
-      <div className={`navbar-menu relative h-full ${sidebarToggle ? 'block' : 'hidden'}`}>
-        <nav
-          id="sidebar"
-          className="flex flex-col pt-6 sm:max-w-xs lg:w-64 w-full"
-        >
+    <div className={`fixed z-20 top-0 left-0 h-full transition-all duration-300 ${sidebarToggle ? 'w-40 md:w-80 bg-slate-800' : 'w-0'}`}>
+      <div className={`navbar-menu relative h-full overflow-hidden ${sidebarToggle ? 'block' : 'hidden'}`}>
+        <nav id="sidebar" className="flex flex-col pt-6 sm:max-w-xs lg:w-64 w-full">
           <div className="px-4">
             <ul className="mb-8 text-sm font-medium mt-7 text-nowrap">
               <li>
@@ -21,8 +19,7 @@ const Sidebar = ({ sidebarToggle }) => {
                   to="/"
                 >
                   <FontAwesomeIcon icon={faHome} />
-                  <span className="select-none">Нүүр Хуудас</span>
-                  
+                  <span className={`select-none ${sidebarToggle ? 'visible' : 'invisible'}`}>Нүүр Хуудас</span>
                 </Link>
               </li>
               <li>
@@ -31,7 +28,7 @@ const Sidebar = ({ sidebarToggle }) => {
                   to="/devices"
                 >
                   <FontAwesomeIcon icon={faMicrochip} />
-                  <span className="select-none">Төхөөрөмж</span>
+                  <span className={`select-none ${sidebarToggle ? 'visible' : 'invisible'}`}>Төхөөрөмж</span>
                 </Link>
               </li>
               <li>
@@ -40,7 +37,7 @@ const Sidebar = ({ sidebarToggle }) => {
                   to="/about"
                 >
                   <FontAwesomeIcon icon={faCircleInfo} />
-                  <span className="select-none">Мэдээлэл</span>
+                  <span className={`select-none ${sidebarToggle ? 'visible' : 'invisible'}`}>Мэдээлэл</span>
                 </Link>
               </li>
               <li>
@@ -49,7 +46,7 @@ const Sidebar = ({ sidebarToggle }) => {
                   to="/contact"
                 >
                   <FontAwesomeIcon icon={faPhone} />
-                  <span className="select-none">Холбогдох</span>
+                  <span className={`select-none ${sidebarToggle ? 'visible' : 'invisible'}`}>Холбогдох</span>
                 </Link>
               </li>
             </ul>
